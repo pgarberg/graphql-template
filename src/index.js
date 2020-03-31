bdba4fdeda31856ba41c521175346c5fb3131828
Peder Garberg <pgarberg@gmail.com>
2020-03-30 20:05:59 -0700

    Fixed typo in index.js - blocking server from starting

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-require('dotenv').config({path:'variables.env'})
-const createServer = require('./createServer')
-const db = require('./db')
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
 
-const server = createServer()
+const server = createServer();
 
 ///TODOs for adding middleware
 
-server.start({
-	cors: {
-		credentials: true,
-		origin: process.env.FRONTEND_URL
-	}
-
-},  deets => {
-	console.log(`Server is up and running on port http://localhost:${deets.port}`)
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(
+      `Server is up and running on port http://localhost:${deets.port}`
+    );
+  }
+);
