c54c34760a185d15e9b2ce8e952d28f4896c95f2
Peder Garberg <pgarberg@gmail.com>
2020-03-30 18:17:23 -0700

    Added some necessary files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// let's go!
+require('dotenv').config({path:'variables.env'})
+const createServer = require('./createServer')
+const db = require('./db')
+
+const server = createServer()
+
+///TODOs for adding middleware
+
+server.start({
+	cors: {
+		credentials: true,
+		origin: process.env.FRONTEND_URL
+	}
+
+},  deets => {
+	console.log(`Server is up and running on port http://localhost:${deets.port}`)
